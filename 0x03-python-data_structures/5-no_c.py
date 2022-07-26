0862a1c8ae438fbb2078dcbaf3d29828dccc4f3b
Emmanuel Owoicho Dickson <eodickson14@gmail.com>
2022-07-26 14:43:51 +0000

    Can you C me now?

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -4,5 +4,5 @@ def no_c(my_string):
     new_list = list(my_string)
     for char in new_list:
         if char == 'c' or char == 'C':
-            del char
-    return ''.join(new_list)
+            new_list.remove(char)
+    print("{:s}".format(''.join(new_list))
