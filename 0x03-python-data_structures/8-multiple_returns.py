83bf50f7b5f371cd0bedaf2a5fc2ffec9905c1f2
Emmanuel Owoicho Dickson <eodickson14@gmail.com>
2022-07-27 09:31:54 +0000

    More returns!

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -5,5 +5,4 @@ def multiple_returns(sentence):
         length, first = 0, None
     else:
         length, first = len(sentence), sentence[0]
-    new_tuple = (length, first)
-    print("Length: {:d} - First character: {}".format(new_tuple[0], new_tuple[1]))
+    return (length, first)
