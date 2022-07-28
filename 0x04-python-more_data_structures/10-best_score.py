8440ce0efdbbf100eb006902f278f36aca493d83
Emmanuel Owoicho Dickson <eodickson14@gmail.com>
2022-07-28 20:09:35 +0000

    Best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    highest = float(-inf)
-    key = ''
+    highest = -float('inf')
+    key = None
     for k,v in a_dictionary.items():
         if v > highest:
+            highest = v
             key = k
-        else:
-            key = None
     return key
