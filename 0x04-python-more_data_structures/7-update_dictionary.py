b5397865f7a508d53a5a19687ee5c017acdacdef
Emmanuel Owoicho Dickson <eodickson14@gmail.com>
2022-07-28 19:23:24 +0000

    Update dictionary

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 
 def update_dictionary(a_dictionary, key, value):
-    for k in list(a_dictionary):
-        if not key:
-            a_dictionary.setdefault(key, value)
-        a_dictionary[key] = value
+    a_dictionary[key] = value
     return a_dictionary
