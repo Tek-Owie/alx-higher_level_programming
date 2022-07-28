667d6a82b2a04a22c862e540c44c33bc6aa01c48
Emmanuel Owoicho Dickson <eodickson14@gmail.com>
2022-07-28 11:49:21 +0000

    Search and replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -3,6 +3,6 @@
 def search_replace(my_list, search, replace):
     new_list = my_list.copy()
     for i in new_list:
-        if new_list[i] == search:
-            new_list[i] = replace
+        if i == search:
+            new_list[new_list.index(i)] = replace
     return new_list
