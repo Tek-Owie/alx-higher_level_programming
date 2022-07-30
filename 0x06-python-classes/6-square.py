2d4ae3f84470c5f51256353f407560a69d7af646
Emmanuel Owoicho Dickson <eodickson14@gmail.com>
2022-07-30 13:55:25 +0000

    Coordinates of a square

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""Defines a square class."""
+
+
+class Square:
+    """Defines a class of square."""
+    def __init__(self, size=0, position=(0,0)):
+        """Initializes an instance with private attr."""
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """It returns the size of the square."""
+        return self.__size
+
+    @property
+    def position(self):
+        """It returns the position of the square."""
+        return (self.__position)
+
+    @size.setter
+    def size(self, size):
+        """sets the size of the square."""
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = size
+
+    @position.setter
+    def position(self, position):
+        """sets the position of the square."""
+        for tup in range(len(position)):
+            if position[tup] < 0:
+                raise TypeError('position must be a tuple of 2 positive integers')
+        else:
+            self.__position = position
+
+    def area(self):
+        """Returns the area of the square."""
+        return (self.__size) ** 2
+
+    def my_print(self):
+        """prints the area with #"""
+        if self.__size == 0:
+            print()
+            return
+        else:
+            for i in range(self.__position[1]):
+                print()
+            for j in range(self.__size):
+                print(" " * self.__position[0], end='')
+                print('#' * self.__size)
