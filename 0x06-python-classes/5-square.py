c2588fe99c88ac922491b1e4d253cd887c267801
Emmanuel Owoicho Dickson <eodickson14@gmail.com>
2022-07-30 12:04:56 +0000

    Printing a square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Defines a square class."""
+
+
+class Square:
+    """Defines a class of square."""
+    def __init__(self, size=0):
+        """Initializes an instance with private attr."""
+        self.size = size
+
+    @property
+    def size(self):
+        """It returns the size of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        """sets the size of the square."""
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = size
+
+    def area(self):
+        """Returns the area of the square."""
+        return (self.__size) ** 2
+
+    def my_print(self):
+        """prints the area with #"""
+        for size in self.area:
+            for j in self.__size:
+                print('#')
