8502be532f04175e37929b43d4167e257698390d
Emmanuel Owoicho Dickson <eodickson14@gmail.com>
2022-08-02 11:29:02 +0000

    Say my name

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This module contains 
+the say_my_name function
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """Return first_name and last_name
+    :first_name - str arg
+    :last_name - str arg
+    :type - str
+    """
+    if type(first_name) is not str:
+        raise TypeError('first_name must be a string ')
+    if type(last_name) is not str:
+        raise TypeError('last_name must be a string ')
+    print("My name is {} {}".format(first_name, last_name))
