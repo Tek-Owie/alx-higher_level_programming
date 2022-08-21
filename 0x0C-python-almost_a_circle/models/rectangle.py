51ca388c7f8e19e2c494ef9c8606ed9fc4c1ea3d
Emmanuel Owoicho Dickson <eodickson14@gmail.com>
2022-08-21 17:53:37 +0000

    First Rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -10,11 +10,19 @@ class Rectangle(Base):
     """This class inherits from Base."""
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        """Initialize the Rectangle class."""
+        """Initialize the Rectangle class.
 
-        self.width = width
-        self.height = height
-        self.x = x
-        self.y = y
+        Args:
+            width (int): The width of the new Rectangle.
+            height (int): The height of the new Rectangle.
+            x (int): The x coordinate of the new Rectangle
+            y (int): The y coordinate of the new Rectangle.
+            id (int): The identity of the new Rectangle.
+        """
+
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
         super().__init__(id)
 
