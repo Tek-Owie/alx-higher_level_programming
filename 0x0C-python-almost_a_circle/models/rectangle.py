317da145ca4bf4d16cfa6f71dc2227bb03206942
Emmanuel Owoicho Dickson <eodickson14@gmail.com>
2022-08-21 18:00:16 +0000

    First Rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -26,3 +26,50 @@ class Rectangle(Base):
         self.__y = y
         super().__init__(id)
 
+    @property
+    def width(self):
+        """Return the value of width."""
+
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Set the value of width."""
+
+        self.__width = value
+
+    @property
+    def height(self):
+        """Return the value of height."""
+
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set the value of height."""
+
+        self.__height = value
+
+    @property
+    def x(self):
+        """Return the value of x."""
+
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """Set the value of x."""
+
+        self.__x = value
+
+    @property
+    def y(self):
+        """Return the value of y."""
+
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """Set the value of y."""
+
+        self.__y = value
