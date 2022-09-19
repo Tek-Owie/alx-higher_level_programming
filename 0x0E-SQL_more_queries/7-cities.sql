f1896241e764d454b3475ca0d3d9c456812cc73b
Emmanuel Owoicho Dickson <eodickson14@gmail.com>
2022-09-19 12:29:44 +0000

    Cities table

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -3,4 +3,5 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
 	id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
 	state_id INT NOT NULL, FOREIGN KEY(state_id) REFERENCES states(id)
-);
+)
+type=InnoDB;
