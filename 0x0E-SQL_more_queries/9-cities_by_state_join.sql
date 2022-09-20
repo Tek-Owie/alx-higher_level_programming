adf2b4d04818073b7fb3fcea79a51bc6694cf233
Emmanuel Owoicho Dickson <eodickson14@gmail.com>
2022-09-20 18:55:10 +0000

    Cities by States

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,7 +1,6 @@
 -- lists all cities contained in the database hbtn_0d_usa.
-SELECT id, name, name
+SELECT cities.id, cities.name, states.name
 FROM cities
 INNER JOIN states
     ON cities.state_id = states.id
-WHERE state_id = 1
 ORDER BY cities.id ASC;
