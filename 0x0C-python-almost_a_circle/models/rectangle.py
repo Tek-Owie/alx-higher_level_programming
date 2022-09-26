b98933c7d699336454cbc48d8adf0d8fa3b7e509
Emmanuel Owoicho Dickson <eodickson14@gmail.com>
2022-09-26 17:15:28 +0000

    __str__

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -29,7 +29,7 @@ class Rectangle(Base):
     def __str__(self):
         """Return the string representation of the rectangle."""
 
-        return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - \
+        return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - 
                 {self.__width}/{self.__height}"
 
     @property
