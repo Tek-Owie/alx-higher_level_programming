30058c6d1c30f73fdeb5190d1daa143e6f60f7e0
Emmanuel Owoicho Dickson <eodickson14@gmail.com>
2022-09-26 19:47:55 +0000

    And now, the Square!

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-#square.py
+# square.py
 """
 This module contains the Square class which 
 inherits from the Rectangle class.
 """
-
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """Inherit from Rectangle."""
+    """Represent the Square class which inherit from Rectangle."""
 
     def __init__(self, size, x=0, y=0, id=None):
         """Initialize a Square instance.
@@ -17,14 +16,11 @@ class Square(Rectangle):
             size - positive integer
             x - positive integer
             y - positive integer
+            id - positive integer
         """
-
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
         """Return a str representation of the Square instance."""
-
-        return "[Square] ({}) {}/{} - {}".format(self.id,
-                                                 self.x,
-                                                 self.y,
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
                                                  self.width)
